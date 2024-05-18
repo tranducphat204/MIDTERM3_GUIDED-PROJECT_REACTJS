c48640078eb7f8cefdca2cfb12e225907938035c
Tran Duc Phat <144576717+tranducphat204@users.noreply.github.com>
2024-05-18 16:55:17 +0700

    fetch api by axios_done (#3)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import './App.css';
-import Navbar from './components/layout/Navbar';
-
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import { useEffect } from "react";
+import axios from "axios";
 function App() {
+  useEffect(() => {
+    const fetchData = async () => {
+      await axios
+        .get("https://api.github.com/users")
+        .then(function (response) {
+          // xử trí khi thành công
+          console.log("GitHub Users:", response.data);
+        })
+        .catch(function (error) {
+          // xử trí khi bị lỗi
+          console.error("Error fetching data:", error);
+        });
+    };
+    fetchData();
+  }, []);
+
   return (
-      <div className="App">
+    <div className="App">
       <Navbar />
-        <div className="container">
-          <h1>Hello from react</h1>
-        </div>
+      <div className="container">
+        <h1>Hello from react</h1>
       </div>
+    </div>
   );
 }
 
