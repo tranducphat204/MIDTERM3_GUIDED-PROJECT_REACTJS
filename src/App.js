a1b07ddea8b5025f6f9f073c8c10ac8e192c3b54
Tran Duc Phat <144576717+tranducphat204@users.noreply.github.com>
2024-05-18 21:16:18 +0700

    ui/show-client_done (#4)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
+import Users from "./components/users/Users";
 function App() {
+  const [users, setUsers] = useState([]);
+
   useEffect(() => {
     const fetchData = async () => {
       await axios
         .get("https://api.github.com/users")
         .then(function (response) {
           // xử trí khi thành công
+          setUsers(response.data);
           console.log("GitHub Users:", response.data);
         })
         .catch(function (error) {
@@ -23,8 +27,9 @@ function App() {
     <div className="App">
       <Navbar />
       <div className="container">
-        <h1>Hello from react</h1>
+        <h1>Github Users Data</h1>
       </div>
+      <Users users={users} />
     </div>
   );
 }
