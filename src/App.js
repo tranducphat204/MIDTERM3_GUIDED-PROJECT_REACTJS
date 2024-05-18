46cf9ffc81377af73e493774392bd3e16cf8d948
Tran Duc Phat <144576717+tranducphat204@users.noreply.github.com>
2024-05-18 21:34:56 +0700

    feat/search-component_done (#5)

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/layout/Navbar";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Users from "./components/users/Users";
+import Search from "./components/users/Search";
 function App() {
   const [users, setUsers] = useState([]);
 
@@ -28,6 +29,7 @@ function App() {
       <Navbar />
       <div className="container">
         <h1>Github Users Data</h1>
+        <Search />
       </div>
       <Users users={users} />
     </div>
