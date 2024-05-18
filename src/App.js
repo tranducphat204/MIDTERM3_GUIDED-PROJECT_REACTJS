df6026d52d13b50995ef45d1399df0cebcaafdd0
Tran Duc Phat <144576717+tranducphat204@users.noreply.github.com>
2024-05-18 22:09:37 +0700

    UI/not found (#8)
    * ui/about_done
    
    * ui/about_done
    
    * ui/not found_done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
+import About from "./components/pages/About";
+import NotFound from "./components/pages/NotFound";
 import Search from "./components/users/Search";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 function App() {
@@ -12,6 +14,8 @@ function App() {
           <h1>Github Users Data</h1>
           <Switch>
             <Route exact path="/" component={Search} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/*" component={NotFound} />
           </Switch>
         </div>
       </Router>
